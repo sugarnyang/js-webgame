c55a22a67601c14bbce3589c4ea0f502ed0090cc
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-03-02 23:40:26 +0900

    ES2020 강좌 2-2. 끝말잇기 구현하기와 주석

---
+++ wordrelay/index.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#button');
+// document.querySelector(아이디)는 그 아이디의 태그를 가져온다.
+btn.addEventListener('click', () => {
+    const word = document.querySelector('#word').textContent;
+    const input = document.querySelector('#input').value;
+    input[0]; //첫글자
+})
