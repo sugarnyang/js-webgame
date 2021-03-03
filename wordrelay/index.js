944aed02d6c23f73b5bc46586812eff2333f28e8
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-03-03 23:49:11 +0900

    ES2020 강좌 2-4. 끝말잇기 구현하기2

--- wordrelay/index.js
+++ wordrelay/index.js
@@ -4,6 +4,26 @@ btn.addEventListener('click', () => {
     const word = document.querySelector('#word').textContent;
     const input = document.querySelector('#input').value;
     const lastIndex = word.length - 1;
-    word[lastIndex]; //끝글자
-    input[0]; //첫글자
+    const w = word[lastIndex]; //끝글자
+    const i = input[0]; //첫글자
+    
+    // 값이 같은가는 === 이다. 
+    if (w === i) { // 같으면
+        document.querySelector('#word').textContent = input;
+        // 제시어 칸의 textContent에 input값을 넣어준다.
+        
+        document.querySelector('#error').textContent = ''; //빈칸으로 만들어준다.
+        document.querySelector('#input').value = ''; //input은 value
+
+        document.querySelector('#input').focus(); //깜빡깜빡
+
+    } else {
+        document.querySelector('#error').textContent = '땡';
+        
+        document.querySelector('#input').value = '';
+
+        document.querySelector('#input').focus(); //깜빡깜빡
+
+        
+    }
 })
