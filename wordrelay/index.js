de7d2fdbddba3b3148c0c0724934dd94ae98e06d
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-03-04 23:36:13 +0900

    ES2020 강좌 2-5. 변수 값 수정과 상수

--- wordrelay/index.js
+++ wordrelay/index.js
@@ -1,29 +1,33 @@
 const btn = document.querySelector('#button');
 // document.querySelector(아이디)는 그 아이디의 태그를 가져온다.
 btn.addEventListener('click', () => {
-    const word = document.querySelector('#word').textContent;
-    const input = document.querySelector('#input').value;
+    const wordTag = document.querySelector('#word');
+    const word = wordTag.textContent;
+
+    const inputTag = document.querySelector('#input');
+    const input = inputTag.value;
+    
+    const errorTag = document.querySelector('#error');
+
     const lastIndex = word.length - 1;
     const w = word[lastIndex]; //끝글자
     const i = input[0]; //첫글자
     
     // 값이 같은가는 === 이다. 
     if (w === i) { // 같으면
-        document.querySelector('#word').textContent = input;
+        wordTag.textContent = input;
         // 제시어 칸의 textContent에 input값을 넣어준다.
         
-        document.querySelector('#error').textContent = ''; //빈칸으로 만들어준다.
-        document.querySelector('#input').value = ''; //input은 value
+        errorTag.textContent = ''; //빈칸으로 만들어준다.
+        inputTag.value = ''; //input은 value
 
-        document.querySelector('#input').focus(); //깜빡깜빡
+        inputTag.focus(); //깜빡깜빡
 
     } else {
-        document.querySelector('#error').textContent = '땡';
+        errorTag.textContent = '땡';
         
-        document.querySelector('#input').value = '';
+        inputTag.value = '';
 
-        document.querySelector('#input').focus(); //깜빡깜빡
-
-        
+        inputTag.focus(); //깜빡깜빡
     }
 })
