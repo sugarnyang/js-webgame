309f15baeb8ef5b5fa7526341e4ea02e6415974b
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-03-05 23:54:12 +0900

    ES2020 강좌 2-7. 강좌 마무리 (중복단어 사용제한)

--- wordrelay/index.js
+++ wordrelay/index.js
@@ -1,33 +1,35 @@
-const btn = document.querySelector('#button');
+const button = document.querySelector('#button');
 // document.querySelector(아이디)는 그 아이디의 태그를 가져온다.
-btn.addEventListener('click', () => {
-    const wordTag = document.querySelector('#word');
-    const word = wordTag.textContent;
 
+const dictionary = [];
+button.addEventListener('click', () => {
+    const wordTag = document.querySelector('#word');
     const inputTag = document.querySelector('#input');
-    const input = inputTag.value;
-    
     const errorTag = document.querySelector('#error');
 
-    const lastIndex = word.length - 1;
-    const w = word[lastIndex]; //끝글자
-    const i = input[0]; //첫글자
-    
-    // 값이 같은가는 === 이다. 
-    if (w === i) { // 같으면
-        wordTag.textContent = input;
-        // 제시어 칸의 textContent에 input값을 넣어준다.
-        
-        errorTag.textContent = ''; //빈칸으로 만들어준다.
-        inputTag.value = ''; //input은 value
+    const word = wordTag.textContent;
+    const input = inputTag.value;
 
-        inputTag.focus(); //깜빡깜빡
+    if (dictionary.includes(input)) {
+        errorTag.textContent = '중복입니다';
 
     } else {
-        errorTag.textContent = '땡';
-        
-        inputTag.value = '';
-
-        inputTag.focus(); //깜빡깜빡
+        // 값이 같은가는 === 이다. 
+        if (word[word.length - 1] === input[0]) { // 같으면
+            // 제시어 칸의 textContent에 input값을 넣어준다.
+            wordTag.textContent = input;
+            errorTag.textContent = 'error view'; //빈칸으로 만들어준다.
+            inputTag.value = ''; //input은 value
+            inputTag.focus(); //깜빡깜빡
+            dictionary.push(input);
+            document.querySelector('#dictionary').textContent = '나온 단어: ' + dictionary;
+        } else {
+            errorTag.textContent = '땡';
+            inputTag.value = '';
+            inputTag.focus(); //깜빡깜빡
+            document.querySelector('#dictionary').textContent = '나온 단어: ' + dictionary;
+        }
     }
+
+    
 })
