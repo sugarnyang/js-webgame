15dbe5df4d4da9f94336be82fc09124915ffbb75
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-03-07 23:23:15 +0900

    ES2020 강좌 3-2. 계산기 구현하기(상수와 변수)
    클릭 이벤트는 언제 일어날지 모르기 때문에, 순서도에서 나중에 그려준다.

--- calculator/index.js
+++ calculator/index.js
@@ -5,4 +5,23 @@ const minusButton = document.querySelector('#minus');
 const divideButton = document.querySelector('#divide');
 const multiplyButton = document.querySelector('#multiply');
 const calculateButton = document.querySelector('#calculate');
-const resultInput = document.querySelector('#result');
+const resultInput = document.querySelector('#result'); // 태그는 객체 
+
+let temp; // 임시값
+let operator; // 연산자
+
+plusButton.addEventListener('click', () => {
+    if (numberInput.value) {
+        temp = numberInput.value;
+        operator = '+';
+    }
+})
+
+clearButton.addEventListener('click', () => {
+
+})
+
+resultButton.addEventListener('click', () => {
+
+})
+
