6ee6eefbfe28673e2570ff0f49c90e78dfdf1c61
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-03-11 22:54:50 +0900

    ES2020 강좌 4-1. 숫자야구 기획하기

---
+++ numberbaseball/index.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('#input');
+const check = document.querySelector('#check');
+const logs = document.querySelector('#logs');
+
+check.addEventListener('click', () => {
+
+});
