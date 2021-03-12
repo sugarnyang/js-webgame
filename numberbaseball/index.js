09a1a3fe920e143a398b0af8cb5ac821585c56a2
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-03-12 23:37:29 +0900

    ES2020 강좌 4-2. 숫자야구 순서도 그리기

--- numberbaseball/index.js
+++ numberbaseball/index.js
@@ -2,6 +2,7 @@ const input = document.querySelector('#input');
 const check = document.querySelector('#check');
 const logs = document.querySelector('#logs');
 
+let count = 0;
 check.addEventListener('click', () => {
 
 });
