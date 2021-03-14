66a510d245a2e48acc5cbb0922c8bb04f7b7b783
sugarnyang <72382334+sugarnyang@users.noreply.github.com>
2021-03-14 23:09:03 +0900

    ES2020 강좌 4-4. 랜덤한 숫자 뽑기

--- numberbaseball/index.js
+++ numberbaseball/index.js
@@ -2,10 +2,18 @@ const input = document.querySelector('#input');
 const check = document.querySelector('#check');
 const logs = document.querySelector('#logs');
 
+let answer;
+const first = String(Math.floor(Math.random() * 10)) // 3
+const second = String(Math.floor(Math.random() * 10)) // 0
+const third = String(Math.floor(Math.random() * 10)) // 4
+const fourth = String(Math.floor(Math.random() * 10)) // 6
+first + second + third + fourth // 3046
+
 let count = 0;
 check.addEventListener('click', () => {
-    const value = input.value;
-    if (value && value.length === 4) {
+    const value = input.value; //문자열 ex) '3046' true truthy
+    // 0, '', NaN, false, null, undefined -> falsy value
+    if (value && value.length === 4) { // 앰퍼센드 AND 연산자 / 보호연산자
         if (answer === value) {
 
         }
