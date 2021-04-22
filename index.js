b36ea9a631db41acde10ca8d8e16ffb0c6ed7012
sugarnyang <midjhincarry@gmail.com>
2021-04-22 23:43:46 +0900

    메인페이지에 모든 게임 넣기 2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const calculatorButton = document.querySelector('#calculator');
 const gugudanButton = document.querySelector('#gugudan');
+const lottoButton = document.querySelector('#lotto');
+const minesweeperButton = document.querySelector('#minesweeper');
 
 calculatorButton.addEventListener('click', () => {
     window.location.href = './calculator/index.html'
@@ -7,4 +9,12 @@ calculatorButton.addEventListener('click', () => {
 
 gugudanButton.addEventListener('click', () => {
     window.location.href = './gugudan/index.html'
+});
+
+lottoButton.addEventListener('click', () => {
+    window.location.href = './lotto/index.html'
+});
+
+minesweeperButton.addEventListener('click', () => {
+    window.location.href = './minesweeper/index.html'
 });
